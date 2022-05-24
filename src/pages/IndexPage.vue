64707fc69649a1d5c40101da9573a3645442e11e
Christian Arlt <developer@chris.arlt.io>
2022-05-24 07:13:16 +0200

    + First investigations on pulling public CVE information

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,9 +9,37 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
+import Http from 'axios';
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  mounted() {
+    this.syncInformation();
+  },
+
+  methods: {
+
+    async syncInformation()
+    {
+      // Note: As all of these websites seem to use combinations of CORS-restrictions,
+      // we will need to build a simple php middleware for pulling the requests without browser
+      // interaction
+
+
+      //https://www.cve-search.org/api/
+      let cveList = await Http.get("https://cve.circl.lu/api/last");
+
+      console.log(cveList);
+
+      let knownExploitedCVECat = await Http.get("https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json");
+
+      console.log(knownExploitedCVECat);
+    },
+
+  },
+
+
 })
 </script>
